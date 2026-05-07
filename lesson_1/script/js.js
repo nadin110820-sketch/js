30e43315341b89348e345fd89d270a7fabc2aea8
Nadia Lynovitskaya <nadin110820@gmail.com>
2026-05-07 10:17:43 +0300

    Add files via upload

---
+++ lesson_1/script/js.js
@@ -0,0 +1,83 @@
+// console.log("hello js")
+// // службовий елемент
+// console.info("hello js")
+// // те саме
+// console.error("hello js")
+// // трапився ерор
+// console.warn("hello js")
+
+// var myFirstVar;
+// // створення змінної верблюжа
+// myFirstVar = 10;
+// console.log(myFirstVar);
+//
+// var mySecondVar = '2';
+// console.log("Змінна:" + mySecondVar);
+// console.log(mySecondVar + myFirstVar);
+// console.log(myFirstVar - mySecondVar);
+
+// var first, second = 2, "4";
+
+// var int = 10;
+// var float = 10.2;
+// var BOOL = true;
+// var str = "str";
+// var nothing;
+// nothing = null;
+// // false=nothing,null ................
+
+// num = 2
+// let num;
+// // let - shows you your mistakes, var - no
+// console.log(num);
+
+// var num = 15;
+// var num = 13;
+// // will write 13
+// let num = 15;
+// let num = 13;
+// // mistake
+// console.log(num);
+
+// let num = 13;
+// num = "14"
+// console.log(num);
+// const Pi = 3.14;
+
+// математичні оператори
+// let a = 21, b = 4;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+
+// let a, b;
+// a = 2 + 4;
+// b = 3 * (a = 10 + 3);
+
+// a = a + 1;
+// a += 1;
+// a++;
+// // інкримент
+// a--;
+// // декримент
+
+// ++a;
+// --a;
+
+// let a = 2, b;
+// b = a++
+// console.log(a);
+
+// let a = '2', b = '3';
+// console.log((a - 0) + (b - 0));
+// // перевести в формат числа
+// console.log(Number(a) + Number(b));
+
+// Щеплення/ інтерполяція
+let name = 'Nadia';
+console.log('Hello ' + name + '!');
+let skill = 'WEB';
+console.log(`Привіт ${name}! Твій найкращий скіл - ${skill} ${2 + 4}`);
